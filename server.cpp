d664e49012ca5acb48ac2fd9b716a03b4c7fc249
Yixing Lao <yixing.lao@gmail.com>
2021-01-17 01:56:05 -0800

    asio

--- server.cpp
+++ server.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#define ASIO_STANDALONE  // Use ASIO standalone lib instead of boost.
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
 int main() {
     std::cout << "I am server." << std::endl;
     return 0;
