b2720d8521039ad28c93d9f43c42f68d811eb0e0
Yixing Lao <yixing.lao@gmail.com>
2021-01-17 20:11:44 -0800

    clean up

--- client.cpp
+++ client.cpp
@@ -5,25 +5,6 @@
 
 #include "webrtc_manager.h"
 
-// void OnWebSocketMessage(WebSocketClient* ws_client,
-//                         websocketpp::connection_hdl hdl,
-//                         WebSocketClient::message_ptr message_ptr) {
-//     const std::string message = message_ptr->get_payload();
-//     std::cout << "[Client] Received message: " << message << std::endl;
-// }
-
-// void OnWebSocketOpen(WebSocketClient* ws_client,
-//                      websocketpp::connection_hdl hdl) {
-//     std::cout << "[Client::OnWebSocketOpen]" << std::endl;
-//     ws_client->send(hdl, "Hello from Client.",
-//                     websocketpp::frame::opcode::text);
-// }
-
-// void OnWebSocketClose(WebSocketClient* ws_client,
-//                       websocketpp::connection_hdl hdl) {
-//     std::cout << "[Client::OnWebSocketClose]" << std::endl;
-// }
-
 using websocketpp::lib::bind;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
@@ -53,11 +34,6 @@ public:
 
     virtual ~WebSocketClientManager() {}
 
-    WebRTCManager InitWebRTCManager() {
-        // TODO: implement this.
-        return WebRTCManager();
-    }
-
     static void OpenHandler(WebSocketClient* ws_client,
                             websocketpp::connection_hdl hdl) {
         std::cout << "[WebSocketClientManager::OpenHandler]" << std::endl;
@@ -85,35 +61,5 @@ private:
 
 int main() {
     WebSocketClientManager ws_client_manager("ws://localhost:8888");
-    WebRTCManager rtc_manager = ws_client_manager.InitWebRTCManager();
-    rtc_manager.Send("Hello world from client!");
     return 0;
 }
-
-// int main() {
-//     using websocketpp::lib::bind;
-//     using websocketpp::lib::placeholders::_1;
-//     using websocketpp::lib::placeholders::_2;
-//
-//     std::cout << "[Client] I am the client." << std::endl;
-//
-//     // TODO: Add close connection.
-//     WebSocketClient ws_client_;
-//     ws_client_.init_asio();
-//     ws_client_.set_open_handler(bind(OnWebSocketOpen, &ws_client_, _1));
-//     ws_client_.set_close_handler(bind(OnWebSocketOpen, &ws_client_, _1));
-//     ws_client_.set_message_handler(bind(OnWebSocketMessage, &ws_client_, _1,
-//     _2));
-//
-//     // Create a connection to the given URI and queue it for connection once
-//     // the event loop starts.
-//     std::string uri = "ws://localhost:8888";
-//     websocketpp::lib::error_code ec;
-//     WebSocketClient::connection_ptr con = ws_client_.get_connection(uri, ec);
-//     ws_client_.connect(con);
-//     ws_client_.run();
-//
-//     std::cout << "[Client] Process ends." << std::endl;
-//
-//     return 0;
-// }
