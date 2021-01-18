430881911d9af494cf48491d7369bfef40888e39
Yixing Lao <yixing.lao@gmail.com>
2021-01-18 02:42:58 -0800

    better bind

--- server.cpp
+++ server.cpp
@@ -14,10 +14,13 @@ class WebSocketServerManager {
 
 public:
     WebSocketServerManager(uint16_t port) : port_(port), ws_server_() {
-        ws_server_.set_open_handler(bind(OpenHandler, &ws_server_, _1));
-        ws_server_.set_close_handler(bind(CloseHandler, &ws_server_));
+        ws_server_.set_open_handler(bind(&WebSocketServerManager::OpenHandler,
+                                         this, &ws_server_, _1));
+        ws_server_.set_close_handler(
+                bind(&WebSocketServerManager::CloseHandler, this, &ws_server_));
         ws_server_.set_message_handler(
-                bind(MessageHandler, &ws_server_, _1, _2));
+                bind(&WebSocketServerManager::MessageHandler, this, &ws_server_,
+                     _1, _2));
         ws_server_.init_asio();
         ws_server_.set_reuse_addr(true);
         ws_server_.listen(port_);
@@ -27,20 +30,20 @@ public:
 
     virtual ~WebSocketServerManager() {}
 
-    static void OpenHandler(WebSocketServer* ws_server,
-                            websocketpp::connection_hdl hdl) {
+    void OpenHandler(WebSocketServer* ws_server,
+                     websocketpp::connection_hdl hdl) {
         std::cout << "[WebSocketServerManager::OpenHandler]" << std::endl;
         ws_server->send(hdl, "Hello from server.",
                         websocketpp::frame::opcode::text);
     }
 
-    static void CloseHandler(WebSocketServer* ws_server) {
+    void CloseHandler(WebSocketServer* ws_server) {
         std::cout << "[WebSocketServerManager::CloseHandler]" << std::endl;
     }
 
-    static void MessageHandler(WebSocketServer* ws_server,
-                               websocketpp::connection_hdl hdl,
-                               WebSocketServer::message_ptr message_ptr) {
+    void MessageHandler(WebSocketServer* ws_server,
+                        websocketpp::connection_hdl hdl,
+                        WebSocketServer::message_ptr message_ptr) {
         const std::string message = message_ptr->get_payload();
         std::cout << "[WebSocketServerManager::MessageHandler] Received "
                      "message: "
