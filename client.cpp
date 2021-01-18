98ecb5281180eff54b80337e2e9f20a891eeab87
Yixing Lao <yixing.lao@gmail.com>
2021-01-17 19:46:45 -0800

    bind one

--- client.cpp
+++ client.cpp
@@ -24,18 +24,34 @@
 //     std::cout << "[Client::OnWebSocketClose]" << std::endl;
 // }
 
+using websocketpp::lib::bind;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+
 class WebSocketClientManager {
     using WebSocketClient =
             websocketpp::client<websocketpp::config::asio_client>;
 
 public:
-    WebSocketClientManager(const std::string& uri) : uri_(uri) {}
+    WebSocketClientManager(const std::string& uri) : uri_(uri), ws_client_() {
+        ws_client_.init_asio();
+        ws_client_.set_open_handler(bind(OnWebSocketOpen, &ws_client_, _1));
+    }
+
     WebRTCManager InitWebRTCManager() { return WebRTCManager(); }
+
+    static void OnWebSocketOpen(WebSocketClient* ws_client,
+                                websocketpp::connection_hdl hdl) {
+        std::cout << "[Client::OnWebSocketOpen]" << std::endl;
+        ws_client->send(hdl, "Hello from Client.",
+                        websocketpp::frame::opcode::text);
+    }
 
     virtual ~WebSocketClientManager() {}
 
 private:
     std::string uri_;
+    WebSocketClient ws_client_;
 };
 
 int main() {
@@ -53,20 +69,20 @@ int main() {
 //     std::cout << "[Client] I am the client." << std::endl;
 //
 //     // TODO: Add close connection.
-//     WebSocketClient ws_client;
-//     ws_client.init_asio();
-//     ws_client.set_open_handler(bind(OnWebSocketOpen, &ws_client, _1));
-//     ws_client.set_close_handler(bind(OnWebSocketOpen, &ws_client, _1));
-//     ws_client.set_message_handler(bind(OnWebSocketMessage, &ws_client, _1,
+//     WebSocketClient ws_client_;
+//     ws_client_.init_asio();
+//     ws_client_.set_open_handler(bind(OnWebSocketOpen, &ws_client_, _1));
+//     ws_client_.set_close_handler(bind(OnWebSocketOpen, &ws_client_, _1));
+//     ws_client_.set_message_handler(bind(OnWebSocketMessage, &ws_client_, _1,
 //     _2));
 //
 //     // Create a connection to the given URI and queue it for connection once
 //     // the event loop starts.
 //     std::string uri = "ws://localhost:8888";
 //     websocketpp::lib::error_code ec;
-//     WebSocketClient::connection_ptr con = ws_client.get_connection(uri, ec);
-//     ws_client.connect(con);
-//     ws_client.run();
+//     WebSocketClient::connection_ptr con = ws_client_.get_connection(uri, ec);
+//     ws_client_.connect(con);
+//     ws_client_.run();
 //
 //     std::cout << "[Client] Process ends." << std::endl;
 //
