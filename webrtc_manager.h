486d03c0ec1506f068c3b38b525d3fdfbb38a9be
Yixing Lao <yixing.lao@gmail.com>
2021-01-17 19:34:09 -0800

    add classes

--- webrtc_manager.h
+++ webrtc_manager.h
@@ -1,2 +1,17 @@
 #pragma once
 
+#include <iostream>
+
+#define ASIO_STANDALONE  // Use ASIO standalone lib instead of boost.
+#include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+
+class WebRTCManager {
+public:
+    WebRTCManager() {}
+    virtual ~WebRTCManager() {}
+
+    void Send(const std::string& message) {
+        std::cout << "WebRTCManager::Send " << message << std::endl;
+    }
+};
